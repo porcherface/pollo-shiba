dcbfbb386395895e6345842a27fb5cb701c4a238
Edoardo "Nazza <edoardo.mazzaracchio@gmail.com>
2021-05-28 16:00:13 +0200

    altra cacata per la esp32 che emette il laser
    è da inserire tutta la parte di EspNOW o di TCP/IP

---
+++ cogito_laser_test_sender/cogito_laser_test_sender.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
