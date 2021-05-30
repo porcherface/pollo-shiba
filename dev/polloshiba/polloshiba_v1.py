e64cb30abe8383f128575ad770231eb49033d55b
andrew <porcherface@gmail.com>
2021-05-30 22:35:17 +0200

    bugfix

--- dev/polloshiba/polloshiba_v1.py
+++ dev/polloshiba/polloshiba_v1.py
@@ -48,10 +48,14 @@ pygame.mixer.init()
 pygame.display.init()
 pygame.font.init() 
 
+# preloading fonts really speeds up the process during execution (LOL)
+bestfont = pygame.font.SysFont('Comic Sans MS', 30)
+
 # program icon
 icon_path = os.path.join(MAIN_PATH,"game", "graphics","res","icon.png")
 program_icon = pygame.image.load(icon_path)
 pygame.display.set_icon(program_icon)
+
 
 # station test-call
 #Interface()
@@ -59,8 +63,16 @@ pygame.display.set_icon(program_icon)
 # MAIN CODE
 if __name__ == "__main__":
   print(pollo)
+  while "all night long":
+
+    playername = input("player name: ")
+    lives = int( input("lives: "))  
+    
+
+
+    result = NewGame(playername, lives, "competition")
 
 # start a single session (player, lives, game_mode)
 # but only if paghi dieci euri
-NewGame("porcherface", 3, "competition")
+
 
